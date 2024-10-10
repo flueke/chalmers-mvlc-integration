300938dd2af6b7c0e63f48bb29b8ae9709637888
Florian Lüke <f.lueke@mesytec.com>
2024-10-10 17:21:19 +0200

    small steps to really make things work again :)

--- build-daq0.sh
+++ build-daq0.sh
@@ -1,15 +1,10 @@
 set -e
-#set -x
+set -x
 
-MAKEJOBS=32
-MVLC_CONF_ARGS="-DMVLC_BUILD_TESTS=OFF -DMVLC_BUILD_CONTROLLER_TESTS=OFF -DMVLC_BUILD_DEV_TOOLS=OFF -DMVLC_BUILD_DOCS=OFF"
-MVLC_SRC="external/mesytec-mvlc"
-export MVLC_DIR="install-prefix"
-export MVLCC_CONFIG="external/mvlcc/bin/mvlcc-config.sh"
-
-cmake -DCMAKE_BUILD_TYPE=Release -S $MVLC_SRC -B $MVLC_SRC/build \
-      -DCMAKE_INSTALL_PREFIX=$MVLC_DIR $MVLC_CONF_ARGS
-cmake --build $MVLC_SRC/build -j$MAKEJOBS --target install
+# Clean some of the repos to remove build artifacts.
+for d in external/{drasi,nurdlib,ucesb,mvlcc}; do
+      cd $d && git clean -d -f && cd -
+done
 
 # Create symlinks to libs in /sources. Having another subdir in-between did
 # break the build.
@@ -17,12 +12,21 @@ ln -sf external/drasi
 ln -sf external/nurdlib
 ln -sf external/ucesb
 
+MAKEJOBS=32
+MVLC_CONF_ARGS="-DMVLC_BUILD_TESTS=OFF -DMVLC_BUILD_CONTROLLER_TESTS=OFF -DMVLC_BUILD_DEV_TOOLS=OFF -DMVLC_BUILD_DOCS=OFF"
+MVLC_SRC="external/mesytec-mvlc"
+export MVLC_DIR="$PWD/install-prefix"
+export MVLCC_CONFIG="external/mvlcc/bin/mvlcc-config.sh"
+
+cmake -DCMAKE_BUILD_TYPE=Release -S $MVLC_SRC -B $MVLC_SRC/build \
+      -DCMAKE_INSTALL_PREFIX=$MVLC_DIR $MVLC_CONF_ARGS
+cmake --build $MVLC_SRC/build -j$MAKEJOBS --target install
+
+echo "MVLC_DIR=$MVLC_DIR"
+
 make -j$MAKEJOBS -C external/mvlcc
 make -j$MAKEJOBS -C drasi
 make -j$MAKEJOBS -C drasi showconfig showconfig_all
-
-# This is too much at times :)
-sed -i -e '/pedantic-errors/d' nurdlib/Makefile
 
 export MVLCC_CFLAGS=$(external/mvlcc/bin/mvlcc-config.sh --cflags)
 export MVLCC_LIBS=$(external/mvlcc/bin/mvlcc-config.sh --libs)
@@ -36,7 +40,6 @@ make -j$MAKEJOBS -C nurdlib showconfig
 # Skipping ucesb for now. For some reason it builds when the tree is clean. The
 # second time around it starts to run an hbook/example/ext_writer_test that
 # never returns. git clean and/or make clean-all did not help.
-#cd ucesb && git clean -d -f && cd ..
 #make -j$MAKEJOBS -C ucesb all-clean
 #make -j$MAKEJOBS -C ucesb empty
 
